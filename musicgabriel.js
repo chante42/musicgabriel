23268f3259199b2f8f0ec7fe74d542fb8dde6822
Olivier Chanteloup <chante42@gmail.com>
2013-10-03 23:34:50 +0200

    modified:   musicgabriel.js 	Ajout de la fonction mylog et modif pour l'utiliser a la place de 	console.log 	Ajout de la commande : repete

--- musicgabriel.js
+++ musicgabriel.js
@@ -4,7 +4,6 @@ var lasttitre;
 
 exports.action = function(data, callback, config, SARAH){
     var fs  = require('fs'); 
-	console.log("lancement de music Gabriel");
 	var config = config.modules.musicgabriel;
     var path = config.path;
     var answers = config.answers;
@@ -14,14 +13,20 @@ exports.action = function(data, callback, config, SARAH){
     // les fonctions doivent etre définies avant les appels
 
     // 
+    // function : mylog
+    //
+    var mylog = function(msg) {
+        console.log("       : " + msg);
+    }
+
+    // 
     // function : choixMusic
     //
     var choixMusic = function(callback, cb) {
-        console.log("music Gabriel : path :" + path);
-        // Callback with TTS
+        mylog("path :" + path);
+
         var answers = config.answers.split('|');
         var answer = answers[ Math.floor(Math.random() * answers.length)]; 
-
 
         fs.readdir(path,function(error,directoryObject)   { 
             nombre_histoire=directoryObject.length;
@@ -31,12 +36,9 @@ exports.action = function(data, callback, config, SARAH){
                 
                 titre = directoryObject[histoire_selectione];
             } while (titre === lasttitre);
-
            
-            
-            
             playMusic(answer, titre, callback, function() {
-                console.log("music Gabriel : callback play");
+                mylog("callback play");
             })
         });
     }
@@ -44,24 +46,34 @@ exports.action = function(data, callback, config, SARAH){
     // 
     // function : playMusic
     //
-    var playMusic = function(answer, titre, callback, cb ) {
+    var playMusic = function(answer, titre, callback, cb ) { 
         lasttitre = titre;
 
         // nettoie le titre de ce qui ne se dit pas
         tmp = titre
         tmp = tmp.replace(".mp3", "");
         tmp = tmp.replace(/^[0-9]*/g, "");
-        console.log("music Gabriel :titre = "  + tmp);
+        mylog("titre = "  + tmp);
         answer = answer + tmp; 
-        
 
-        console.log(answer);
+        mylog(answer);
         callback({'tts': answer});
-        
         
         SARAH.play(path+ titre);     
     }   
-        
+    
+    // 
+    // function : erreurLastTitre
+    //
+    var erreurLastTitre = function(cmd, callback, cb ) {
+        mylog(" " +cmd +" -> je ne connais pas la chanson precedente");
+        callback({'tts': 'je ne connais pas la chanson précédente'});
+    }
+
+
+    console.log("   ");    
+    console.log("music Gabriel : ");
+    console.log("---------------");
 
 	if (!config.prenom) {
 		callback({'tts':'Paramètre prénom invalide'});
@@ -70,24 +82,36 @@ exports.action = function(data, callback, config, SARAH){
   	
     if (data.titredemande == 'suivant') {
         if (lasttitre !== undefined) {
-            console.log("music Gabriel : suivant");
+            mylog(" suivant");
             SARAH.pause(path+ lasttitre);
             choixMusic(callback, function() {});
         } else {
-            console.log("music Gabriel : je ne connais pas la chanson précédente"});
-            callback({'tts':'suivant : je ne connais pas la chanson précédente'});
+            erreurLastTitre(data.titredemande ,callback);
+        }
+        return;
+    } 
+    else if (data.titredemande == 'repete') {
+        if (lasttitre !== undefined) {
+            titre = lasttitre;
+            mylog(" repete");
+            SARAH.pause(path+ lasttitre);
+            answer = "je répète :";
+
+            playMusic(answer, titre, callback, function() {
+            });
+        } else {
+            erreurLastTitre(data.titredemande ,callback);
         }
         return;
     } 
     
     else if (data.titredemande === 'stop') {
         if (lasttitre !== undefined) {
-            console.log("music Gabriel : stop");
+            mylog(" stop");
             SARAH.pause(path+ lasttitre);
             callback({'tts':'stop'});
         } else {
-            console.log("music Gabriel : je ne connais pas la chanson précédente"});
-            callback({'tts':'stop : je ne connais pas la chanson précédente'}) 
+            erreurLastTitre(data.titredemande ,callback);
         }    
         return;
     }
@@ -107,7 +131,7 @@ exports.action = function(data, callback, config, SARAH){
     }
     
     else if (data.titredemande === 'aleatoire') {
-        console.log("music Gabriel : aléatoire");
+        mylog(" aléatoire");
         choixMusic(callback, function() {});
     }  
     else {
